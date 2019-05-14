aefda5a855ac9fd1488e31d07f6631950a9f4509
Katherine Klasnich <katherineklasnich@gmail.com>
2019-05-14 12:36:12 +0100

    routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,28 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :cocktails, only:[:new, :show, :create, :index] do
+    resources :doses, only:[:new, :create]
+  end
+
+  resources :doses, only:[:destroy]
+
+
+  # # A user can see the list of cocktails
+  # GET "cocktails", to: "cocktails#index"
+
+  # # A user can see the details of a given cocktail, with the dose needed for each ingredient
+  # GET "cocktails/:id", to: "cocktails/show"
+
+  # # A user can create a new cocktail
+  # GET "cocktails/new", to: "cocktails#new"
+  # POST "cocktails", to: "cocktails#create"
+
+  # # A user can add a new dose (ingredient/description pair) to an existing cocktail
+  # GET "cocktails/:id/doses/new", to: "doses#new"
+  # POST "cocktails/:id/doses", to: "doses#create"
+
+  # # A user can delete a dose that belongs to an existing cocktail. How can we make a delete link again?
+  # DELETE "doses/:id", to: "cocktails#destroy"
+
 end
